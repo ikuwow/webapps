75131632c47694f0cba45d2449c7e277c907628b
Ikuo Degawa <degawa@ikuwow.com>
2015-03-16 00:32:42 +0900

    Add basic informations and dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name             'webapps'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'ikuwow'
+maintainer_email 'degawa@ikuwow.com'
+depends          'httpd', '~> 0.2.6'
 license          'All rights reserved'
 description      'Installs/Configures webapps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
