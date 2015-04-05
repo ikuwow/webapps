86edeb0a1d6a278de699107e381e7ca3e91b885e
Ikuo Degawa <degawa@ikuwow.com>
2015-04-05 20:57:45 +0900

    Modify test code to ignore ubuntu php checking

--- test/integration/default/serverspec/php_spec.rb
+++ test/integration/default/serverspec/php_spec.rb
@@ -3,6 +3,10 @@ set :backend, :exec
 
 php_version = '5.6.7'
 
-describe command("/usr/local/bin/php -v | grep #{php_version}") do
-    its(:exit_status) { should eq 0 }
+puts os[:family]
+
+if ["centos", "redhat", "debian"].include?(os[:family])
+    describe command("/usr/local/bin/php -v | grep #{php_version}") do
+        its(:exit_status) { should eq 0 }
+    end
 end
