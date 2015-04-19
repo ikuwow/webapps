ca56d9f029775ac181ba93751a9d5c86bf5af50c
Ikuo Degawa <degawa@ikuwow.com>
2015-04-19 22:51:03 +0900

    Install ssl certificates

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -11,3 +11,23 @@ include_recipe 'webapps::default'
 
 include_recipe 'apache2::mod_ssl'
 
+directory "#{node['apache']['dir']}/keys" do
+    owner "root"
+    group "root"
+    action :create
+end
+
+ssl_certificates = Chef::EncryptedDataBagItem.load('apache','ssl_certificates')
+
+{
+    "certificate" => "#{node['apache']['dir']}/keys/wildcard.ikuwow.com.crt",
+    "key" => "#{node['apache']['dir']}/keys/wildcard.ikuwow.com.key"
+}.each do |name,path|
+    file path do
+        owner 'root'
+        group 'root'
+        mode '0644'
+        content ssl_certificates[name]
+        action :create
+    end
+end
