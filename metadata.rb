fc5c81655af0dedf942f947f2582bbb8fea39931
Ikuo Degawa <degawa@ikuwow.com>
2015-04-25 10:58:06 +0900

    Bump up version 0.6.3

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ depends          'build-essential', '~> 2.2.2'
 license          'All rights reserved'
 description      'Installs/Configures webapps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.2'
+version          '0.6.3'
