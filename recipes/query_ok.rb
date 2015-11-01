f07614645167ee38deeedd437b2aa5850d4146c0
Ikuo Degawa <degawa@ikuwow.com>
2015-11-01 19:11:13 +0900

    change scheme of git (https)

--- recipes/query_ok.rb
+++ recipes/query_ok.rb
@@ -13,7 +13,7 @@ include_recipe 'apache2::mod_expires'
 app_dir = "/var/www/html/query_ok"
 
 git 'query_ok' do
-    repository "git@github.com:ikuwow/query_ok.git"
+    repository "https://github.com/ikuwow/query_ok.git"
     revision "master"
     checkout_branch "release"
     destination app_dir
